340326d4d7d1f67fac98f783918fdd8540986cc8
Sandro2468 <laksandror@gmail.com>
2024-01-25 15:25:39 +0700

    p

--- helpers/convertDate.js
+++
@@ -1,8 +0,0 @@
-function convertDate(since) {
-    const dated = new Date(since);
-    const options = {year: 'numeric', month: 'long', day: 'numeric' };
-    return dated.toLocaleDateString('id-ID', options)  
-}
-
-module.exports = convertDate
-
