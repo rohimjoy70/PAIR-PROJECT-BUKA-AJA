b6b85fe8769a5c0a1fd71c8adea18959c006b970
rohimjoy70 <rohiimjoy70@gmail.com>
2024-01-25 18:51:23 +0700

    commit belum final

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -4,88 +4,92 @@ const convertDate = require("../helpers/convertDate");
 const { Op } = require("sequelize");
 
 class Controller {
-   static listAllProduct(req, res) {
-      const search = req.query.search;
-      let where = {};
+   static async listAllProduct(req, res) {
+      try {
+         const search = req.query.search;
+         let where = {};
 
-      if (search) {
-         where.name = {
-            [Op.iLike]: `%${search}%`,
-         };
+         if (search) {
+            where.name = {
+               [Op.iLike]: `%${search}%`,
+            };
+         }
+
+         const data = await Product.findAll({
+            where,
+            include: Store,
+         });
+
+         res.render("allProduct", { data });
+      } catch (err) {
+         res.send(err);
       }
-      Product.findAll({
-         where,
-         include: Store,
-      })
-         .then((data) => {
-            res.render("allProduct", { data });
-         })
-         .catch((err) => {
-            res.send(err);
-         });
    }
 
-   static buyProduct(req, res) {
-      let id = +req.params.id;
-      Product.decrement({ stock: 1 }, { where: { id } })
-         .then((_) => {
-            res.redirect(`/store`);
-         })
-         .catch((err) => {
-            res.send(err);
-         });
+   static async buyProduct(req, res) {
+      try {
+         const id = +req.params.id;
+
+         await Product.decrement({ stock: 1 }, { where: { id } });
+
+         res.redirect(`/store`);
+      } catch (err) {
+         res.send(err);
+      }
    }
 
-   static addProduct(req, res) {
-      Store.findAll()
-         .then((Store) => {
-            res.render("addProduct", { Store });
-         })
-         .catch((err) => {
-            res.send(err);
-         });
+   static async addProduct(req, res) {
+      try {
+         const Store = await Store.findAll();
+
+         res.render("addProduct", { Store });
+      } catch (err) {
+         res.send(err);
+      }
    }
 
-   static saveProduct(req, res) {
-      const { name, description, price, stock, StoreId, imgURL } = req.body;
-      const Nprice = Number(price);
-      const Nstock = Number(stock);
-      Product.create({
-         name,
-         description,
-         price: Nprice,
-         stock: Nstock,
-         StoreId,
-         imageURL:imgURL,
-      })
-         .then((data) => {
-            res.redirect(`/store/${StoreId}`);
-         })
-         .catch((err) => {
-            const error = {};
-            if (err.name === "SequelizeValidationError") {
-               err.errors.forEach((el) => {
-                  if (error[el.path]) {
-                     error[el.path].push(el.message);
-                  } else {
-                     error[el.path] = [el.message];
-                  }
-               });
-            }
-            console.log(err)
-            res.send(err);
+   static async saveProduct(req, res) {
+      try {
+         const { name, description, price, stock, StoreId, imgURL } = req.body;
+         const Nprice = Number(price);
+         const Nstock = Number(stock);
+
+         await Product.create({
+            name,
+            description,
+            price: Nprice,
+            stock: Nstock,
+            StoreId,
+            imageURL: imgURL,
          });
+
+         res.redirect(`/store/${StoreId}`);
+      } catch (err) {
+         const error = {};
+         if (err.name === "SequelizeValidationError") {
+            err.errors.forEach((el) => {
+               if (error[el.path]) {
+                  error[el.path].push(el.message);
+               } else {
+                  error[el.path] = [el.message];
+               }
+            });
+         }
+         console.log(err);
+         res.send(err);
+      }
    }
 
-   static deleteProduct(req, res) {
-      let id = +req.params.id;
-      Product.destroy({ where: { id } })
-         .then((_) => {
-            res.redirect(`/store`);
-         })
-         .catch((err) => {
-            res.send(err);
-         });
+   static async deleteProduct(req, res) {
+      try {
+         const id = +req.params.id;
+
+         await Product.destroy({ where: { id } });
+
+         res.redirect(`/store`);
+      } catch (err) {
+         res.send(err);
+      }
    }
 }
 
--- controller/StoreController.js
+++ controller/StoreController.js
@@ -4,87 +4,79 @@ const convertDate = require("../helpers/convertDate");
 const { Op } = require("sequelize");
 
 class Controller {
-   static listStore(req, res) {
-      const msg = req.query.msg;
-      let store;
-      Store.findAll()
-         .then((data) => {
-            store = data;
-            return Profile.findAll();
-         })
-         .then((data1) => {
-            res.render("listStore", { store, convertDate, data1, msg });
-         })
-         .catch((err) => {
-            res.send(err);
-         });
+   static async listStore(req, res) {
+      try {
+         const msg = req.query.msg;
+         let store;
+         store = await Store.findAll();
+         const data1 = await Profile.findAll();
+         res.render("listStore", { store, convertDate, data1, msg });
+      } catch (err) {
+         res.send(err);
+      }
    }
 
-   static listProductAtStore(req, res) {
-      const id = +req.params.id;
-      Product.findAll({
-         include: Store,
-         where: {
-            StoreId: id,
-         },
-      })
-         .then((data) => {
-            const money = new Product();
-            const store = data[0].Store;
-            res.render("productByStore", { data, store, money });
-         })
-         .catch((err) => {
-            res.send(err);
+   static async listProductAtStore(req, res) {
+      try {
+         const id = +req.params.id;
+         const data = await Product.findAll({
+            include: Store,
+            where: {
+               StoreId: id,
+            },
          });
+         const money = new Product();
+         const store = data[0].Store;
+         res.render("productByStore", { data, store, money });
+      } catch (err) {
+         res.send(err);
+      }
    }
 
-   static addStore(req, res) {
-      const category = Store.category;
-      User.findAll()
-         .then((data) => {
-            res.render("addStore", { category, data });
-         })
-         .catch((err) => {
-            res.send(err);
-         });
+   static async addStore(req, res) {
+      try {
+         const category = Store.category;
+         const data = await User.findAll();
+         res.render("addStore", { category, data });
+      } catch (err) {
+         res.send(err);
+      }
    }
-   static saveStore(req, res) {
-      const { name, category, since, UserId } = req.body;
-      const dated = new Date(since);
-      Store.create({
-         name,
-         category,
-         since: dated,
-         UserId,
-      })
-         .then((data) => {
-            res.redirect("/store");
-         })
-         .catch((err) => {
-            const error = {};
-            if (err.name === "SequelizeValidationError") {
-               err.errors.forEach((el) => {
-                  if (error[el.path]) {
-                     error[el.path].push(el.message);
-                  } else {
-                     error[el.path] = [el.message];
-                  }
-               });
-            }
-            res.send(error);
+
+   static async saveStore(req, res) {
+      try {
+         const { name, category, since, UserId } = req.body;
+         const dated = new Date(since);
+         await Store.create({
+            name,
+            category,
+            since: dated,
+            UserId,
          });
+         res.redirect("/store");
+      } catch (err) {
+         const error = {};
+         if (err.name === "SequelizeValidationError") {
+            err.errors.forEach((el) => {
+               if (error[el.path]) {
+                  error[el.path].push(el.message);
+               } else {
+                  error[el.path] = [el.message];
+               }
+            });
+         }
+         res.send(error);
+      }
    }
 
    static async deleteStore(req, res) {
       try {
          const storeId = req.params.id;
-
          await Store.destroy({
             where: {
                id: storeId,
             },
          });
-
          res.redirect("/store");
       } catch (err) {
          res.send(err);
